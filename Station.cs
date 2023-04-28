3da7c6b91de4a0c4ca6ebc4143859ca837ba5a4d
skebis <keranen.emil@gmail.com>
2023-04-28 23:51:34 +0300

    Added properties for station.

--- Station.cs
+++ Station.cs
@@ -2,5 +2,10 @@
 {
     public class Station
     {
+        public Guid StationId { get; set; }
+
+        public string StationName { get; set; }
+
+        public string StationAddress { get; set; }
     }
 }
